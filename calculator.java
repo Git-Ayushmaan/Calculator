c866f5f19dcae4094d272e50719a55933d3f07d8
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-06 22:16:21 +0530

    Subtraction functionality added

--- calculator.java
+++ calculator.java
@@ -8,14 +8,35 @@ public class Calculator{
                 double num1 = sc.nextDouble();
                 System.out.print("Enter second number: ");
                 double num2 = sc.nextDouble();
+		
+	 	System.out.println("Enter choice.");
+		System.out.println("1.Add");
+		System.out.println("2.Subtract");
+		int choice = sc.nextInt();
+		double result;
 
-                double result = addition(num1, num2);
-                System.out.println("The addition of two nos is: " + result);
+		switch(choice){
+		case 1:
+                result = add(num1, num2);
+                System.out.println("Result: " + result);
+                break;
+            	case 2:
+                result = subtract(num1, num2);
+                System.out.println("Result: " + result);
+                break;
+               
+          	default:
+                System.out.println("Invalid choice. Please try again.");
+		}
 
                 sc.close();
         }
 
-        public static double addition(double num1, double num2) {
+        public static double add(double num1, double num2) {
                 return num1 + num2;
         }
+	public static double subtract(double num1, double num2) {
+                return num1 - num2;
+        }
+
 }
